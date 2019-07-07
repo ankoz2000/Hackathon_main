eea8a0ea991e60be51b2d22ddfa85f0ad1ce8180
Andrew <52471156+ankoz2000@users.noreply.github.com>
2019-07-07 13:16:12 +0300

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,28 @@
+
+	function BlogGood1() {
+	location.href='./data/Fact.html';
+	}
+	
+	function BlogGood2() {
+	location.href='./data/GRP.html';
+	}
+	
+	function BlogGood3() {
+	location.href='./data/trud.html';
+	}
+
+	function BlogGood4() {
+	location.href='./data/vibitie.html';
+	}
+
+	function BlogGood5() {
+	location.href='./data/prognoz.html';
+	}
+
+	function BlogGood6() {
+	location.href='./data/potrebnost.html';
+  }
+
+  function GetHomePage() {
+  	location.href='../index.html';
+  }
